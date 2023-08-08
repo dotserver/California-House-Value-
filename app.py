5392c4ece675b9bc08f1d11440540fdd6f263ab1
Dotserver <goluwatobisodiq@gmail.com>
2023-08-08 17:40:09 +0100

    web page

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ scalar = pickle.load(open('scaling.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "The House value is {}".format(output))
+
 @app.route('/predict_api', methods = ['POST'])
 def predict_api():
     data = request.json['data']
@@ -23,5 +31,7 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+
 if __name__=="__main__":
     app.run(debug=True)
+    
